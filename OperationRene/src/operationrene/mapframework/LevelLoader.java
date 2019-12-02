efcf7584025c64e8eee9965aa9b7fd241c10b3a8
miky gargiulo <miki.gargiulo@live.it>
2019-12-02 19:13:06 +0100

    2019 12 02 A - STB
    Setup commit

---
+++ OperationRene/src/operationrene/mapframework/LevelLoader.java
@@ -0,0 +1,50 @@
+package operationrene.mapframework;
+
+import operationrene.mapframework.LevelMap;
+import java.io.*;
+import flexjson.*;
+
+public class LevelLoader {
+
+    public static LevelMap loadLevel() {
+        String ls = System.getProperty("line.separator");
+
+        JSONDeserializer deserializer = new JSONDeserializer();
+        StringBuilder sb = new StringBuilder();
+        FileReader fr = null;
+        BufferedReader br = new BufferedReader(fr);
+
+        try {
+
+            String line = "";
+            while((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append(ls);
+            }
+
+            return (LevelMap) deserializer.deserialize(sb.toString());
+        } catch (Exception ex) {
+            System.out.println(ex.getStackTrace());
+        }
+
+        return null;
+    }
+
+    public static String saveLevel(LevelMap level, String path) {
+        JSONSerializer serializer = new JSONSerializer();
+        String json = serializer.serialize(level);
+
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(path);
+            fw.write(json);
+            fw.close();
+            return json;
+        } catch (Exception ex) {
+            System.out.println(ex.getStackTrace());
+        }
+
+        return null;
+    }
+
+}
